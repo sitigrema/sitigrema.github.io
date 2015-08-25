d724b89d5adfa8d4e79fe174f530001963d9c369
Martastain <martas@imm.cz>
2015-08-25 11:06:34 +0200

    build scripts added

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+cd __data
+./mk_products.py
+cd ..
+
+jekyll build
+
 find $DIR/ -type d -exec chmod 775 {} +
 find $DIR/ -type f -exec chmod 664 {} +
 chmod 775 $DIR/deploy.sh
